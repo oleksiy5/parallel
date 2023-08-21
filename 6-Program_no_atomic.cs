b4bad0b69eacc9b42bd6abbd3ad9a24f9b85e0e0
OloTechnic <n.507592979@gmail.com>
2023-08-21 08:33:51 +0200

    Add files via upload

---
+++ 6-Program_no_atomic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+class DedlockExaple
+{
+	static int sec = 2000;
+
+	static int _counter = 0;
+
+        static Random _r;
+
+	static void Main()
+	{
+		_r = new Random();
+		var t1 = new Thread(() => T1("1"));
+		var t2 = new Thread(() => T1("2"));
+
+		t1.Start();
+		t2.Start();
+
+		t1.Join();
+		t2.Join();
+		Console.WriteLine("FInal result: "+_counter);
+	}
+
+
+	static void T1(string tID) 
+	{
+		for(;;)
+		{
+			int v = _counter;
+			v++;
+			_counter = v;	
+			Thread.Sleep(_r.Next(100, 2001));
+			Console.WriteLine("tID=" + tID + ":" + v + ":" + _counter);
+
+			if(_counter >= 5)
+				break;
+		}
+		
+	}
+
+	static int GetRandom() 
+	{
+		return  new Random().Next(1,201);
+	}
+
+}
+
