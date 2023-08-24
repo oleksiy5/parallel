7fdd78bbda3b443af2d66c295e146f08f1369bde
OloTechnic <n.507592979@gmail.com>
2023-08-24 09:18:15 +0200

    Add files via upload

---
+++ deadlock_and_sln-v2/Program_ok_atomic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class DedlockExaple
+{
+	static int _counter = 0;
+
+	static void Main()
+	{
+		int numTasks = 10;
+		Task[] tasks = new Task[numTasks];
+		for(int i=0; i <numTasks; i++)
+		{
+			tasks[i] = Task.Run(() => T1(i.ToString()));
+		}
+		Task.WaitAll(tasks);
+
+		Console.WriteLine("_counter: "+_counter);
+	}
+
+
+	//oczekiwany wynik 10*10000=100 000
+	static void T1(string tID) 
+	{
+		int internalCount = 0;
+		for(int i=0; i<10000; i++)
+		{
+			internalCount++;
+			Interlocked.Increment(ref _counter);
+		}	
+		Console.WriteLine("tID=" + tID + "; internalCount=" + internalCount);
+	}
+}
