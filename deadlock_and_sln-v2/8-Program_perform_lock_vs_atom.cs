5dd8555ac504495e39b74b3e6cca3a982eb5f28a
OloTechnic <n.507592979@gmail.com>
2023-09-07 09:31:52 +0200

    Add files via upload

---
+++ deadlock_and_sln-v2/8-Program_perform_lock_vs_atom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+class DedlockExaple
+{
+	static int _counter = 0;
+	static object _resLock = new object();
+
+	static void Main()
+	{
+		var sw = new Stopwatch();
+		sw.Start();
+		int numTasks = 10;
+		Task[] tasks = new Task[numTasks];
+		for(int i=0; i <numTasks; i++)
+		{
+			tasks[i] = Task.Run(() => T1(i.ToString()));
+		}
+		Task.WaitAll(tasks);
+		sw.Stop();
+		Console.WriteLine("elapsed time is {0} ms", sw.ElapsedMilliseconds);
+		Console.WriteLine("_counter: "+_counter);
+	}
+
+
+	//oczekiwany wynik 10*10000=100 000
+	static void T1(string tID) 
+	{
+		int internalCount = 0;
+		for(int i=0; i<10000; i++)
+		{
+			internalCount++;
+			//v.1
+			//lock(_resLock)
+			//{
+			//	_counter++;
+			//}
+			//v.2
+			Interlocked.Increment(ref _counter);
+		}	
+		Console.WriteLine("tID=" + tID + "; internalCount=" + internalCount);
+	}
+}
